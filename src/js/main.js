a68fde285ae20b26f750f1dac4394bff3bd2abbc
bthrsn <mikhail.lhts@gmail.com>
2020-11-04 17:40:51 +0300

    add phone mask

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import modals from './modules/modal';
 import slider from './modules/slider';
 import forms from './modules/forms';
+import mask from './modules/mask';
 
 window.addEventListener('DOMContentLoaded', () => {
 
@@ -8,4 +9,5 @@ window.addEventListener('DOMContentLoaded', () => {
     slider('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
     slider('.main-slider-item', 'vertical',);
     forms();
+    mask('[name="phone"]');
 });
